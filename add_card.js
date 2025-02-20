4b5109949f7f5b25e99254e1b206892cb6f891bd
Manuel <argument689@gmail.com>
2025-02-20 16:17:19 +0100

    neu

--- add_card.js
+++ add_card.js
@@ -1,17 +1,38 @@
 const form = document.querySelector('[data-js="form"]');
-const card = document.querySelector("#zuweisung");
+const card = document.querySelector('[data-js="zuweisung"]');
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   const formElements = event.target.elements;
 
-  const neu = `<div class="card_form">
-<div class="frage_form">frage</div>
-<button class="button_form">show answer</button>
-<svg class="icon_form" xmlns="http://www.w3.org/2000/svg" width="45" height="45" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" ><path d="m19 21-7-4-7 4V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2v16z"/></svg>
-<div class="tag_form"># tag</div>
-</div>`;
+  const container = document.createElement("div");
+  container.classList.add("card_form");
 
-  card.innerHTML += neu;
+  const icon_form = document.createElement("img");
+  icon_form.classList.add("icon_form");
+  icon_form.src = "bookmark.svg";
+  container.appendChild(icon_form);
+
+  const frage_form = document.createElement("div");
+  frage_form.textContent = `${formElements.frage.value}`;
+  frage_form.classList.add("frage_form");
+  container.appendChild(frage_form);
+
+  const button_form = document.createElement("button");
+  button_form.textContent = "show answer";
+  button_form.classList.add("button_form");
+  container.appendChild(button_form);
+
+  const antwort_form = document.createElement("div");
+  antwort_form.textContent = `${formElements.antwort.value}`;
+  antwort_form.classList.add("antwort_form");
+  container.appendChild(antwort_form);
+
+  const tag_form = document.createElement("div");
+  tag_form.textContent = "###";
+  tag_form.classList.add("tag_form");
+  container.appendChild(tag_form);
+
+  card.appendChild(container);
 });
