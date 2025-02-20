380038325195f6eaf595e009cf83d5ade2713e46
Manuel <argument689@gmail.com>
2025-02-20 10:36:25 +0100

    neu

--- add_card.js
+++ add_card.js
@@ -1,14 +1,17 @@
 const form = document.querySelector('[data-js="form"]');
-const card = document.querySelector('[data-js="section"]');
+const card = document.querySelector("#zuweisung");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   const formElements = event.target.elements;
 
-  const div = document.createElement("div");
-  div.classList.add("card");
-  div.textContent = `Frage:${formElements.frage.value}, Antwort:${formElements.antwort.value},  Tag:${formElements.text.value}`;
+  const neu = `<div class="card_form">
+<div class="frage_form">frage</div>
+<button class="button_form">show answer</button>
+<svg class="icon_form" xmlns="http://www.w3.org/2000/svg" width="45" height="45" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" ><path d="m19 21-7-4-7 4V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2v16z"/></svg>
+<div class="tag_form"># tag</div>
+</div>`;
 
-  card.append(div);
+  card.innerHTML += neu;
 });
